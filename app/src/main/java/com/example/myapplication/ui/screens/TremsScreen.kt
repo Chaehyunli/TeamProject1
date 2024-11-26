1bd3ae84d936538e0054a710b08264fb4889c502
sunm2n <134378502+sunm2n@users.noreply.github.com>
2024-11-26 17:25:42 +0900

    이용약관 페이지에 뒤로가기 버튼 추가

--- app/src/main/java/com/example/myapplication/ui/screens/TremsScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/TremsScreen.kt
@@ -3,28 +3,46 @@ package com.example.myapplication.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.background
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TermsScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "이용약관",
-            fontSize = 24.sp,
-            color = Color.Black
-        )
-    }
+fun TermsScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "이용약관", fontSize = 24.sp, color = Color.Black) },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "뒤로가기")
+                    }
+                },
+                modifier = Modifier
+                    .background(Color.White),
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.White)
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(paddingValues)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+            }
+        }
+    )
 }
